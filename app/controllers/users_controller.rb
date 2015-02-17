522017d92a6bafe50ece94f19b091eecef7aaa88
Kate <kbalducci@hotmail.com>
2015-02-17 17:57:52 -0600

    set up controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :user_name)
+    end
 end
