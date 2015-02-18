854aaaaa794ebb0adc724bc95f842af00021b08c
Kate <kbalducci@hotmail.com>
2015-02-17 21:09:46 -0600

    able to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,31 @@ class UsersController < ApplicationController
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+def index
+  @user = current_user
+  @glows = Glow.all
+end
+
+def new
+  @user = User.new
+  @glow = Glow.new
+end
+
+def create
+  @user = User.new(user_params)
+
+  respond_to do |format|
+    if @user.save
+      format.html { redirect_to @user, notice: 'User was successfully created.' }
+      format.json { render json: @user, status: :created, location: @user }
+    else
+      format.html { render "welcome/index" }
+      format.json { render json: @user.errors, status: :unprocessable_entity }
+    end
+  end
+  end
+
+
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :username, :avatar)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :glows
-  validates_presence_of :user_name, :first_name, :last_name
-  validates_uniqueness_of :user_name
+  validates_presence_of :username, :first_name, :last_name
+  validates_uniqueness_of :username
 
   has_attached_file :avatar,
   :styles => { :medium => "300x300>", :thumb => "100x100>" },
